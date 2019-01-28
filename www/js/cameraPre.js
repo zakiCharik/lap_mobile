5f293985d6dc978b5066a7843cf35a7fc6f64bbe
Zakaria Charik <z.charik@gmail.com>
2019-01-28 17:06:48 +0100

    Logging the code for camera preview first lines

--- www/js/cameraPre.js
+++ www/js/cameraPre.js
@@ -11,11 +11,13 @@ var cameraPreviewGetPicture  = function () {
 	  tapFocus: true,
 	  previewDrag: false
 	};
+	alert('1');
     var flash_mode = 'off';
     var flash_on_btn = document.createElement('img');
     flash_on_btn.src = 'img/flash_on.svg';
     flash_on_btn.className += 'btn_class';
     flash_on_btn.className += ' flash_class'
+	alert('2');
 
     flash_on_btn.onclick = function() {
         flash_mode = 'on';
@@ -24,12 +26,14 @@ var cameraPreviewGetPicture  = function () {
 
         CameraPreview.setFlashMode(flash_mode);
     }
+	alert('3');
 
     var flash_off_btn = document.createElement('img');
     flash_off_btn.src = 'img/flash_off.svg';
     flash_off_btn.className += 'btn_class';
     flash_off_btn.className += ' flash_class'
 
+	alert('4');
     flash_off_btn.onclick = function() {
         flash_mode = 'off';
         flash_off_btn.style.visibility = 'hidden';
@@ -37,12 +41,18 @@ var cameraPreviewGetPicture  = function () {
 
         CameraPreview.setFlashMode(flash_mode);
     }
+	alert('5');
     flash_off_btn.style.visibility = 'hidden';
     
+	alert('6');
     var rect = document.createElement('div');
+
+	alert('7');
+	var take_pic_btn = document.createElement('img');
     take_pic_btn.src = 'img/btn_icon_mini.png';
     rect.className += 'rect_class';
 
+	alert('8');
     // Append to body section
     document.body.appendChild(rect);
     document.body.appendChild(take_pic_btn);
@@ -52,5 +62,6 @@ var cameraPreviewGetPicture  = function () {
     var x_coord = rect_coords.left, y_coord = rect_coords.top;
 
 
+	alert('9');
 	CameraPreview.startCamera(options);
 }
